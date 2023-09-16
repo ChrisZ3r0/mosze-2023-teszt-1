77bf449af5bc3e8219254ad77ab5ccf07947d0c0
Zahuczky <zahuczky@proton.me>
2023-09-16 11:50:24 +0200

    comment out erroneous lines

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    // int *b = new int[NELEMENTS];                 // N_ELEMENTS helyett NELEMENTS
+    // std::cout << '1-100 ertekek duplazasa'       // hiányzó ; karakter
+    // for (int i = 0;)                             // hiányzó for ciklus kondíció
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    // for (int i = 0; i; i++)                      // i-től i-ig megy a loop, hiányzó ; karakter
     {
-        std::cout << "Ertek:"
+        //std::cout << "Ertek:"                     // hiányzó ; karakter
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    // for (int i = 0; i < N_ELEMENTS, i++)         // hiányzó ; karakter, vessző helyett ; karakter
     {
-        atlag += b[i]
+        // atlag += b[i]                            // hiányzó ; karakter
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
