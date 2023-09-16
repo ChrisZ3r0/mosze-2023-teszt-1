683009433f09046269498d76b395038677fcf711
Zahuczky <zahuczky@proton.me>
2023-09-16 11:59:38 +0200

    fix errors

--- main.cpp
+++ main.cpp
@@ -4,23 +4,26 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    // int *b = new int[NELEMENTS];                 // N_ELEMENTS helyett NELEMENTS
-    // std::cout << '1-100 ertekek duplazasa'       // hiányzó ; karakter
-    // for (int i = 0;)                             // hiányzó for ciklus kondíció
+    int *b = new int[N_ELEMENTS];                           // NELEMENT kijavítva N_ELEMENTS-re
+    std::cout << "1-100 ertekek duplazasa" << std::endl;    // hiányzó ; karakter hozzáadva (+ endl mert azt szeretem)
+    for (int i = 0; i < N_ELEMENTS; i++)					// NELEMENT kijavítva N_ELEMENTS-re
     {
         b[i] = i * 2;
     }
-    // for (int i = 0; i; i++)                      // i-től i-ig megy a loop, hiányzó ; karakter
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        //std::cout << "Ertek:"                     // hiányzó ; karakter
-    }    
+        std::cout << "Ertek: " << b[i] << std::endl;		// hiányzó ; karakter és érték kiíratás hozzáadva
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    // for (int i = 0; i < N_ELEMENTS, i++)         // hiányzó ; karakter, vessző helyett ; karakter
+    int atlag = 0;                                          // atlag értékének inicializálása
+    for (int i = 0; i < N_ELEMENTS; i++)                    // vessző kijavítva pontosvesszőre
     {
-        // atlag += b[i]                            // hiányzó ; karakter
+        atlag += b[i];                                      // hiányzó ; kaakter hozzáadva
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+
+    delete[] b;											    // bár úgyis kilép a program, azért szabadítsuk fel a memóriát
+
     return 0;
 }
